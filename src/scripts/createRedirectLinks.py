2097cad484b6211bea81a300fc9c5c0dfcfd0515
Insa Kroeger <24524940+insa-k@users.noreply.github.com>
2018-08-02 10:40:38 +0200

    exclude html and xml files from redirect + add all install packs

--- src/scripts/createRedirectLinks.py
+++ src/scripts/createRedirectLinks.py
@@ -14,17 +14,28 @@ except OSError:
 
 with open(htaccessFile, "a") as f:
     for subdir, dirs, files in os.walk(downloadDir):
+        print(files)
         for file in files:
             fullPath = os.path.join(subdir, file)
             # strip src/site/resources from path
             allDirs = fullPath.split("/")
             # get version if available
             hasVersion = (len(allDirs) == 6)
-            if hasVersion:
+            if file.endswith((".xml", ".html")):
+                continue
+            elif hasVersion:
                 version = allDirs[4]
                 strippedPath = "/" + allDirs[3] + "/" + version + "/" + allDirs[5]
                 if version == "voices":
-                    gitHubURL = "https://github.com/marytts/marytts/releases/download/" + version + "/" + str(file)
+                    if file.endswith(".zip"):
+                        print(str(file))
+                        repoName = re.sub('-3\..+?\.zip', '', file)
+                        if file == "mbrola-de1-3.1.0.zip":
+                            version = "3.1.0"
+                        else:
+                            version = "3.5.0"
+                        print(repoName)
+                        gitHubURL = "https://github.com/marytts/"+ repoName + "/releases/download/" + version + "/" + str(file)
                 else:
                     gitHubURL = "https://github.com/marytts/marytts/releases/download/v" + version + "/" + str(file)
                 # if version != ("v"):
@@ -40,7 +51,21 @@ with open(htaccessFile, "a") as f:
                     strippedPath = "/" + allDirs[3] + "/" + allDirs[4]
 
                 #      extract version number from file name
-                    version = re.sub('\.pack\d+.jar', '', allDirs[4].lstrip("mary-install-"))
+                #     version = re.sub('\.pack\d+.jar', '', allDirs[4].lstrip("mary-install-"))
+                    version = "3.6.0"
+                    gitHubURL = "https://github.com/marytts/marytts/releases/download/v" + version + "/" + str(file)
+
+                    print(version)
+                    print(gitHubURL)
+                    # put path and url in quotes to catch any space problems
+                    htaccessLine = "Redirect 307 \"" + strippedPath + "\" \"" + gitHubURL + "\""
+                    f.write(htaccessLine + "\n")
+                    # if it is a mary-install jar
+                elif file.startswith("mary-standalone"):
+                    print(file)
+                    strippedPath = "/" + allDirs[3] + "/" + allDirs[4]
+
+                    version = "3.6.0"
                     gitHubURL = "https://github.com/marytts/marytts/releases/download/v" + version + "/" + str(file)
 
                     print(version)
@@ -49,4 +74,4 @@ with open(htaccessFile, "a") as f:
                     htaccessLine = "Redirect 307 \"" + strippedPath + "\" \"" + gitHubURL + "\""
                     f.write(htaccessLine + "\n")
                 else:
-                    break
+                    continue
